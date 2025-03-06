0043bbf35c99bde270de2ad3dfce61015f25900e
MasterlyMisheirt <thewizardofmich@gmail.com>
2025-03-06 23:07:39 +0000

    Changed edit and add dialog

--- view/entity/addRecord_dialog.php
+++ view/entity/addRecord_dialog.php
@@ -1,29 +1,29 @@
 <dialog class="dialog" id="add-dialog">
-    <div class="add-container form-wrapper">
-        <h2>Calculate eGFR</h2>
-  <form id="egfr-form" method="POST" action="requestHandler.php?action=addPatientRecord">
-    <div id="input-container">
-      <label for="creatinine">Serum Creatinine</label>
-      <div id="input-content">
-        <input type="text" id="creatinine" name="creatinine" required>
-        <text>micromol/l</text>
-      </div>
-    </div>
-    <div id="input-container">
-      <label for="blood-pressure">Blood Pressure</label>
-      <div id="input-content">
-        <input type="text" id="blood-pressure" name="blood-pressure" required>
-        <text>mmHg</text>
-      </div>
-    </div>
-    <button type="submit">Create record</button>
-  </form>
-</div>
+<div class="add-container form-wrapper">
+				<h2>Add Record</h2>
+				<form
+					method="POST"
+					action="requestHandler.php?action=addPatientRecord"
+				>
+        <div class="form">
+					<label for="creatinine">Serum Creatinine (micromol/l)</label>
+					<input type="hidden" id="record-id" name="record-id" />
+					<input type="text" id="creatinine" name="creatinine" required />
+					<label for="blood-pressure">Blood Pressure (mmHg)</label>
+					<input
+						type="text"
+						id="blood-pressure"
+						name="blood-pressure"
+						required
+					/>
+        </div>
+					<button type="submit" class="green-button">Add</button>
+          <button type="reset" class="red-button" onclick="showAddDialog(false)">Cancel</button>
+				</form>
+			</div>
 </dialog>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 const addDialog = document.getElementById("add-dialog");
-const addWrapper = document.querySelector(".add-container");
 const showAddDialog = (show) => show ? addDialog.showModal() : addDialog.close();
-addDialog.addEventListener("click", (e) => !addWrapper.contains(e.target) && addDialog.close());
 </script>
--- view/entity/editRecord_dialog.php
+++ view/entity/editRecord_dialog.php
@@ -1,30 +1,30 @@
 <dialog class="dialog" id="edit-dialog">
-    <div class="edit-container form-wrapper">
-        <h2>Calculate eGFR</h2>
-  <form id="egfr-form" method="POST" action="requestHandler.php?action=editPatientRecord">
-    <div id="input-container">
-      <label for="creatinine">Serum Creatinine</label>
-      <div id="input-content">
-        <input type="hidden" id="record-id" name="record-id">
-        <input type="text" id="creatinine" name="creatinine" required>
-        <text>micromol/l</text>
-      </div>
-    </div>
-    <div id="input-container">
-      <label for="blood-pressure">Blood Pressure</label>
-      <div id="input-content">
-        <input type="text" id="blood-pressure" name="blood-pressure" required>
-        <text>mmHg</text>
-      </div>
-    </div>
-    <button type="submit">Edit record</button>
-  </form>
-</div>
-</dialog>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
+			<div class="edit-container form-wrapper">
+				<h2>Edit Record</h2>
+				<form
+					method="POST"
+					action="requestHandler.php?action=editPatientRecord"
+				>
+        <div class="form">
+					<label for="creatinine">Serum Creatinine (micromol/l)</label>
+					<input type="hidden" id="record-id" name="record-id" />
+					<input type="text" id="creatinine" name="creatinine" required />
+					<label for="blood-pressure">Blood Pressure (mmHg)</label>
+					<input
+						type="text"
+						id="blood-pressure"
+						name="blood-pressure"
+						required
+					/>
+        </div>
+					<button type="submit" class="green-button">Edit</button>
+          <button type="reset" class="red-button" onclick="showEditDialog(false)">Cancel</button>
+
+				</form>
+			</div>
+		</dialog>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+		<script>
 const editDialog = document.getElementById("edit-dialog");
-const editWrapper = document.querySelector(".edit-container");
 const showEditDialog = (show) => show ? editDialog.showModal() : editDialog.close();
-editDialog.editEventListener("click", (e) => !editWrapper.contains(e.target) && editDialog.close());
 </script>
