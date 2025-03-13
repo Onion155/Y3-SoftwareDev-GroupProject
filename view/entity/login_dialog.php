8a6ea4e140428929dbe7e7e3b564bedc91c1f0ae
MasterlyMisheirt <thewizardofmich@gmail.com>
2025-03-13 11:48:52 +0000

    Implemented show and hide password

--- view/entity/login_dialog.php
+++ view/entity/login_dialog.php
@@ -1,58 +1,56 @@
 <dialog class="dialog" id="login-dialog">
-			<div class="login-container form-wrapper">
-				<a href="#" onclick="showLoginDialog(false)">
-					<img id="x-icon" src="other/x-icon.png" alt="My Kidney Buddy mascot logo">
-				</a>
-				<h3>Login Account</h3>
-				<form class="login-form form" method="POST">
-					<label for="login-email">Email</label>
-					<input
-						name="email"
-						id="email"
-						value="drmarco@gmail.com"
-						type="text"
-						placeholder="Enter your email"
-					/>
-					<label for="login-password">Password</label>
-					<input
-						name="password"
-						id="password"
-						value="123"
-						type="password"
-						placeholder="Enter your password"
-					/>
-					<p class="error-message" id="login-error-message"></p>
-					<button onclick="postLoginDetails()">Log in</button>
-					<div class="link">
-						Added by the doctor? <a
-							href="#"
-							onclick="showLoginDialog(false); showSignupDialog(true);"
-							>Create an account</a>
-					</div>
-				</form>
-			</div>
-		</dialog>
-		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <div class="login-container form-wrapper">
+        <a href="#" onclick="showLoginDialog(false)">
+            <img id="x-icon" src="other/x-icon.png" alt="My Kidney Buddy mascot logo">
+        </a>
+        <h3>Login Account</h3>
+        <form class="login-form form" method="POST">
+            <label for="login-email">Email</label>
+            <input name="email" id="email" value="drmarco@gmail.com" type="text" placeholder="Enter your email" />
+            <label for="login-password">Password</label>
+            <div id="view-password-wrapper">
+                <input name="password" id="password" value="123" type="password" placeholder="Enter your password" />
+                <button id="toggle-password" onclick="togglePassword($(this))" type="button">
+                    <img id="show-hide" src="other/hide.png">
+                </button>
+            </div>
+            <p class="error-message" id="login-error-message"></p>
+            <button onclick="postLoginDetails()">Log in</button>
+            <div class="link">
+                Added by the doctor? <a href="#" onclick="showLoginDialog(false); showSignupDialog(true);">Create an
+                    account</a>
+            </div>
+        </form>
+    </div>
+</dialog>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-const loginDialog = document.getElementById("login-dialog");
-const showLoginDialog = (show) => show ? loginDialog.showModal() : (loginDialog.close(), $(".error-message").text(""));
+    const loginDialog = document.getElementById("login-dialog");
+    const showLoginDialog = (show) => show ? loginDialog.showModal() : (loginDialog.close(), $(".error-message").text(""));
 
-function postLoginDetails() {
-	event.preventDefault();
-$.post("requestHandler.php", {
-	action: "login",
-	email: $("#email").val(),
-	password: $("#password").val()
-}, function (message) {
-	if(message == "success") window.location.href = "dashboard.php";
-	else {
-		if(message != "Please fill in the fields") {
-			$("#email").val("");
-			$("#password").val("");
-		}
-		$("#login-error-message").text(message);
-	}
-});
-}
+    function togglePassword(e) {
+        const passwordField = $(e).closest("#view-password-wrapper").find("input");
+        const showHideIcon = $(e).closest("#view-password-wrapper").find("#show-hide");
+        passwordField.attr("type", (passwordField.attr("type") === "password") ? "text" : "password");
+        showHideIcon.attr("src", (showHideIcon.attr("src") === "other/hide.png") ? "other/show.png" : "other/hide.png");
+    }
+
+    function postLoginDetails() {
+        event.preventDefault();
+        $.post("requestHandler.php", {
+            action: "login",
+            email: $("#email").val(),
+            password: $("#password").val()
+        }, function (message) {
+            if (message == "success") window.location.href = "dashboard.php";
+            else {
+                if (message != "Please fill in the fields") {
+                    $("#email").val("");
+                    $("#password").val("");
+                }
+                $("#login-error-message").text(message);
+            }
+        });
+    }
 </script>
 </div>
--- view/entity/signup_dialog.php
+++ view/entity/signup_dialog.php
@@ -1,51 +1,60 @@
 <dialog class="dialog" id="signup-dialog">
-			<div class="signup-container form-wrapper">
-				<a href="#" onclick="showSignupDialog(false)">
-					<img id="x-icon" src="other/x-icon.png" alt="My Kidney Buddy mascot logo">
-				</a>
-				<h3>Create Account</h3>
-				<form class="signup-form form" method="POST">
-					<label for="signup-email">Email</label>
-					<input name="email" id="signup-email" type="text" placeholder="Enter your email assigned to your doctor"/>
-					<label for="signup-password">Password</label>
-					<input name="password" id="signup-password" type="text" placeholder="Create a password" />
-					<label for="confirm-password">Confirm Password</label>
-					<input
-						name="confirmPassword"
-						id="confirm-password"
-						type="text"
-						placeholder="Repeat created password"
-					/>
-					<p class="error-message" id="signup-error-message"></p>
-					<button onclick="postSignupDetails()">Sign up</button>
-					<div class="link">
-						Already have an account?
-						<a
-							href="#"
-							onclick="showLoginDialog(true); showSignupDialog(false);"
-							>Log in</a
-						>
-					</div>
-				</form>
+	<div class="signup-container form-wrapper">
+		<a href="#" onclick="showSignupDialog(false)">
+			<img id="x-icon" src="other/x-icon.png" alt="My Kidney Buddy mascot logo">
+		</a>
+		<h3>Create Account</h3>
+		<form class="signup-form form" method="POST">
+			<label for="signup-email">Email</label>
+			<input name="email" id="signup-email" type="text" placeholder="Enter your email assigned to your doctor" />
+			<label for="signup-password">Password</label>
+			<div id="view-password-wrapper">
+				<input name="password" id="signup-password" type="password" placeholder="Create a password" />
+				<button id="toggle-password" onclick="togglePassword($(this))" type="button">
+					<img id="show-hide" src="other/hide.png">
+				</button>
 			</div>
-		</dialog>
+			<div id="view-password-wrapper">
+				<label for="confirm-password">Confirm Password</label>
+				<input name="confirmPassword" id="confirm-password" type="password" placeholder="Repeat created password" />
+				<button id="toggle-password" onclick="togglePassword($(this))" type="button">
+					<img id="show-hide" src="other/hide.png">
+				</button>
+			</div>
+
+			<p class="error-message" id="signup-error-message"></p>
+			<button onclick="postSignupDetails()">Sign up</button>
+			<div class="link">
+				Already have an account?
+				<a href="#" onclick="showLoginDialog(true); showSignupDialog(false);">Log in</a>
+			</div>
+		</form>
+	</div>
+</dialog>
 
 
 <script>
-    const signupDialog = document.getElementById("signup-dialog");
-    const showSignupDialog = (show) => show ? signupDialog.showModal() : (signupDialog.close(), $(".error-message").text(""));
+	const signupDialog = document.getElementById("signup-dialog");
+	const showSignupDialog = (show) => show ? signupDialog.showModal() : (signupDialog.close(), $(".error-message").text(""));
 
-function postSignupDetails() {
-	event.preventDefault();
-$.post("requestHandler.php", {
-	action: "signup",
-	email: $("#signup-email").val(),
-	password: $("#signup-password").val(),
-	confirmPassword: $("#confirm-password").val()
-}, function (message) {
-	if(message == "success") window.location.href = "dashboard.php";
-	else $("#signup-error-message").text(message);
-});
-}
+	function togglePassword(e) {
+		const passwordField = $(e).closest("#view-password-wrapper").find("input");
+		const showHideIcon = $(e).closest("#view-password-wrapper").find("show-hide");
+		passwordField.attr("type", (passwordField.attr("type") === "password") ? "text" : "password");
+		showHideIcon.attr("src", (showHideIcon.attr("src") === "other/hide.png") ? "other/show.png" : "other/hide.png");
+	}
+
+	function postSignupDetails() {
+		event.preventDefault();
+		$.post("requestHandler.php", {
+			action: "signup",
+			email: $("#signup-email").val(),
+			password: $("#signup-password").val(),
+			confirmPassword: $("#confirm-password").val()
+		}, function (message) {
+			if (message == "success") window.location.href = "dashboard.php";
+			else $("#signup-error-message").text(message);
+		});
+	}
 </script>
 </div>
